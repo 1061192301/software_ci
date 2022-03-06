50f065f209d39e2cdbc791b51eb05415f266c2f0
1061192301 <75670454+1061192301@users.noreply.github.com>
2022-03-06 14:27:50 +0800

    Update judge.py

--- judge.py
+++ judge.py
@@ -1,4 +1,2 @@
 for i in range(1, 2):
-    f = open('wenfa/testfile%s.txt' % str(i), 'w')
-    f = open('wenfa/input%s.txt' % str(i), 'w')
-    f = open('wenfa/output%s.txt' % str(i), 'w')
+    print(i)
